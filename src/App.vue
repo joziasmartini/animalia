f6f0d4137939546d44f5799dce3a50a7d4644c5b
Jozias Martini Dequi <jozias.martini@pampa.compasso>
2021-03-03 14:35:59 -0300

    Start project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img :src="foto.url" :alt="foto.titulo">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      titulo: 'Alurapic',
+      foto: {
+        url: 'https://source.unsplash.com/random',
+        titulo: 'Unsplash Image'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
