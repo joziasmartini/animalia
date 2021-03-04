64e0050482d338ac2ad9ab683db35171d90e7a5b
joziasmartini <jozias.martini@gmail.com>
2021-03-04 10:25:32 -0300

    Create painel component

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,11 +5,9 @@
     <h1 class="centralizado">{{ titulo }}</h1>    
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2> 
-          <div class="painel-conteudo"></div>
+        <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-        </div>
+        </meu-painel>
       </li>
     </ul>
   </div>
@@ -49,28 +47,5 @@ export default {
   }
   .imagem-responsiva {
     width: 100%;
-  }
-
-  /* Painel */ 
-
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
