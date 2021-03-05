cd2407da4db17de06a56f7d57c3b28a585747f6a
joziasmartini <jozias.martini@gmail.com>
2021-03-05 14:41:34 -0300

    Configure dynamic routes in menu

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="corpo">
+
+    <nav>
+      <ul>       
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+
     <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-export default {
+import { routes }  from './routes';
 
+export default {
+  data() {
+    return {
+      routes
+    }
+  }
 }
 </script>
 
--- app/src/routes.js
+++ app/src/routes.js
@@ -2,6 +2,6 @@ import Cadastro from './components/cadastro/Cadastro.vue'
 import Home from './components/home/Home.vue'
 
 export const routes = [
-    { path: '', component: Home},
-    { path: '/cadastro', component: Cadastro}
+    { path: '', component: Home, titulo: 'Home'},
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro'}
 ];
